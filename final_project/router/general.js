2f04535bfc870040947e065375055529a5e23540
Michael Williams <mgwilliams112@gmail.com>
2026-05-21 19:49:26 -0400

    Implemented tasks 3-6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,27 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    const nameExists = (usesrname) => {
+        sameName = users.filter((user)=> user.username == username);
+        if(sameName.length > 0) {
+            return true
+        } else {
+            return false
+        }
+    }; 
+    if (username && password) {
+        if (!nameExists(username)) {
+            users.push({'username' : username, 'password': password});
+            res.status(200).json({message: 'The user was succesfully registered.'})
+            
+        } else {
+            res.status(404).json({message: 'This username is unavailable.'})
+        }
+    } else {
+        res.status(404).json({message: 'Username and/or password missing.'})
+    }
 });
 
 // Get the book list available in the shop
@@ -24,8 +43,8 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
+    let booksByAuth = [];
     let requestedKeys = Object.keys(books).filter((book) => books[book].author == author);
-    let booksByAuth = []
     requestedKeys.forEach((key)=>{
         booksByAuth.push(books[key])
     })
@@ -34,14 +53,19 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let booksByTitle = [];
+    let requestedKeys = Object.keys(books).filter((book) => books[book].title == title);
+    requestedKeys.forEach((key)=>{
+        booksByTitle.push(books[key])
+    })
+    res.send(booksByTitle)
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const ISBN = req.params.isbn;
+    res.send(books[ISBN].reviews)
 });
 
 module.exports.general = public_users;
