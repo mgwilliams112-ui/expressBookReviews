bf6874d9f602e648ca0eb640e8178c126f1e879f
Michael Williams <mgwilliams112@gmail.com>
2026-05-21 17:52:11 -0400

    initial authentication function added

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,18 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+        jwt.verify(token, 'access', (err, user) => {
+            if(!err) {
+                req.user = user
+            } else {
+                res.send('User not authenticated.')
+            }
+        })
+    } else {
+        res.send('User not logged in.')
+    }
 });
  
 const PORT =5000;
@@ -19,4 +30,4 @@ const PORT =5000;
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log("Server is running at port " + PORT));
