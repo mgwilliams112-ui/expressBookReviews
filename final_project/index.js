0703ef65f51061028d8808dd6b6dffb5e165a41d
Michael <mgwilliams112@gmail.com>
2026-05-22 00:11:01 -0400

    Partial implementation of login, needs fix

--- final_project/index.js
+++ final_project/index.js
@@ -15,13 +15,14 @@ app.use("/customer/auth/*", function auth(req,res,next){
         let token = req.session.authorization['accessToken'];
         jwt.verify(token, 'access', (err, user) => {
             if(!err) {
-                req.user = user
+                req.user = user;
+                next();
             } else {
-                res.send('User not authenticated.')
+                return res.status(403).json({ message: "User not authenticated" })
             }
         })
     } else {
-        res.send('User not logged in.')
+        return res.status(403).json({ message: "User not logged in" })
     }
 });
  
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,44 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{
+    let existingUser = users.filter((user)=> user.username == username);
+    if (existingUser.length > 0) {
+        return true
+    } else {
+        return false
+    }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
+const authenticatedUser = (username,password) => { //returns boolean
 //write code to check if username and password match the one we have in records.
+    let validUser = users.filter((user) => user.username === username && user.password === password);
+    if (validUser.length > 0) {
+        return true
+    } else {
+        return false
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password) {
+        res.status(404).json({message: 'Please double check that username and password are entered.'})
+    } else {
+        if (authenticatedUser(username, password)) {
+           let accessToken = jwt.sign(
+            {data: password},
+            'access',
+            {expiresIn: 60*30}
+           );
+           req.session.authorization = {accessToken, username};
+           return res.status(200).json({message: "You have logged in succesfully!"})
+        } else {
+            return res.status(208).json({ message: "Invalid Login. Check username and password." })
+        }
+    }
 });
 
 // Add a book review
