49dfd7b2c021f402de4da337b2c74e5e9b8ccc86
Michael Williams <mgwilliams112@gmail.com>
2026-05-26 12:18:02 -0400

    Implemented 10-13, implementing async/await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,28 +29,38 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async function (req, res) {
     res.send(JSON.stringify(books, null, 4))
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+async function bookByISBN (ISBN) {
+    return books[ISBN]
+};
+public_users.get('/isbn/:isbn', async function (req, res) {
     const ISBN = req.params.isbn;
-    res.send(books[ISBN])
+    res.send(await bookByISBN(ISBN))
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+async function booksByAuth(author) {
+    let booksAuth = Object.values(books).filter(book => book['author'] === author);
+    return booksAuth
+};
+public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author;
-    let booksByAuth = Object.values(books).filter(book => book['author'] === author);
-    res.send(booksByAuth)
+    res.send(await booksByAuth(author))
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+async function getByTitle(title) {
+    let booksByTitle = Object.values(books).filter((book) => book['title'] == title);
+    return booksByTitle
+}
+public_users.get('/title/:title', async function (req, res) {
     const title = req.params.title;
     let booksByTitle = Object.values(books).filter((book) => book['title'] == title);
-    res.send(booksByTitle)
+    res.send(await getByTitle(title))
 });
 
 //  Get book review
