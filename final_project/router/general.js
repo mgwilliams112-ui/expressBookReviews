04eefb7f03c711d50bf396cd532924309e26cf7e
Michael Williams <mgwilliams112@gmail.com>
2026-05-26 10:41:13 -0400

    Simplified code for get by author and get by title functions

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,6 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -43,22 +42,14 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
-    let booksByAuth = [];
-    let requestedKeys = Object.keys(books).filter((book) => books[book].author == author);
-    requestedKeys.forEach((key)=>{
-        booksByAuth.push(books[key])
-    })
+    let booksByAuth = Object.values(books).filter(book => book['author'] === author);
     res.send(booksByAuth)
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
-    let booksByTitle = [];
-    let requestedKeys = Object.keys(books).filter((book) => books[book].title == title);
-    requestedKeys.forEach((key)=>{
-        booksByTitle.push(books[key])
-    })
+    let booksByTitle = Object.values(books).filter((book) => book['title'] == title);
     res.send(booksByTitle)
 });
 
